4eec77fde37b35ae36e0c8f912a3c1558ed5475e
Nodepond <hello@nodepond.com>
2014-01-26 12:42:48 +0100

    some nore quotes

--- talkengine.rb
+++ talkengine.rb
@@ -7,5 +7,8 @@
 	"The pessimist complains about the wind; the optimist expects it to change; the realist adjusts the sails.",
 	"Honesty is the first chapter in the book of wisdom.",
 	"Silence is a source of great strength.",
-	"The road of excess leads to the palace of wisdom."
+	"The road of excess leads to the palace of wisdom.",
+	"A good head and a good heart are always a formidable combination.",
+	"I'd rather regret the things I've done than regret the things I haven't done.",
+	"Wisdom begins in wonder."
 	]
